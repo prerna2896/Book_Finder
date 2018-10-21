8cc9944eae25b1c71f794b11d8cb49ce0727fa52
Harsh Patwari <patwari.h@gmail.com>
2018-10-21 02:00:57 -0700

    changes

--- src/different_models.py
+++ src/different_models.py
@@ -1,5 +1,19 @@
 import data_extractor as de
+import cross_validation as cv
+from sklearn.tree import DecisionTreeClassifier as DTC
 
 data = de.read_data("../data/book_data.xlsx")
 
-print data
+features = data[:, ["author", "genre"]]
+labels = data["user_rating"]
+
+print features
+print labels
+
+print features.shape
+print labels.shape
+
+# Decision Tree Classifier
+# decisionTree = DTC()
+# cv.leave_k_out_cross_validation(1, decisionTree, features, labels)
+
