0207ade192075121ae63c99dc1c33520b68f2745
Harsh Patwari <hapi@amazon.com>
2018-10-22 22:01:24 -0700

    print

--- src/different_models.py
+++ src/different_models.py
@@ -12,10 +12,10 @@ labels = np.array(rating_data)
 
 # Decision Tree Classifier
 from sklearn.tree import DecisionTreeClassifier as DTC
-print "Decision Tree Classifier accuracy: ", cv.leave_k_out_cross_validation(1, DTC(), features, labels)
+print "Decision Tree Classifier accuracy: \t", cv.leave_k_out_cross_validation(1, DTC(), features, labels)
 
 # KNeighbors Classifier
 from sklearn.neighbors import KNeighborsClassifier as KNC
-print "KNeighbors Classifier accuracy: ", cv.leave_k_out_cross_validation(1, KNC(), features, labels)
+print "KNeighbors Classifier accuracy: \t", cv.leave_k_out_cross_validation(1, KNC(), features, labels)
 
 
