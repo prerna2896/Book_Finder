717924f8029d1cc68e3686e5b82098e689cc6b25
Harsh Patwari <hapi@amazon.com>
2018-10-22 21:41:20 -0700

    removed print lines

--- src/different_models.py
+++ src/different_models.py
@@ -1,6 +1,5 @@
 import data_extractor as de
 import cross_validation as cv
-from sklearn.tree import DecisionTreeClassifier as DTC
 import numpy as np
 
 data = de.read_data("../data/book_data.xlsx")
@@ -11,13 +10,7 @@ features = np.column_stack((author_data, genre_data))
 rating_data = data["user_rating"]
 labels = np.array(rating_data)
 
-print features
-print labels
-
-print features.shape
-print labels.shape
-
 # Decision Tree Classifier
-decisionTree = DTC()
-cv.leave_k_out_cross_validation(1, decisionTree, features, labels)
+from sklearn.tree import DecisionTreeClassifier as DTC
+print cv.leave_k_out_cross_validation(1, DTC(), features, labels)
 
